9354d0faf7f4a04f0ad00358b70250a7cc26e8f7
saya <saya@saya-mac-book.local>
2022-11-22 12:24:58 +0900

    tailwindcss changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,17 @@ module.exports = {
   theme: {
     fontSize: {
       'xxs': '.6rem',
-      'xs': '.75rem', /* 11px */
-      'sm': '.8125rem', /* 0.875rem .8125(13px)に変更 */
-      'tiny': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
+      'xs': '.70rem',
+      'sm': '.70rem',
+      'base': '.70rem',
+      'lg': '.9rem',
+      'xl': '1.125rem',
+      // '2xl': '1.5rem',
+      // '3xl': '1.875rem',
+      // '4xl': '2.25rem',
+      // '5xl': '3rem',
+      // '6xl': '4rem',
+      // '7xl': '5rem',
     },
   },
   variants: {
@@ -24,3 +23,32 @@ module.exports = {
   plugins: [],
 };
 
+// .75 12px
+// .78 12.5px
+// .8125 13px
+
+// main
+// text-base md:text-sm
+
+// Workslist
+// title: text-base
+// more: text-base
+
+// works-single
+// title text-lg
+// text text-base md:text-sm
+
+// profile
+// title-jp text-lg md:text-xl
+// title-en text-base md:text-sm
+
+// contact
+// title: text-xl
+// text text-base md:text-sm
+
+// news
+// newsのみ少し小さめ
+// title: text-sm
+
+// smartphone nav
+// title: text-sm
